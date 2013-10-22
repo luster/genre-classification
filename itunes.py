afe356d32e6a84103d957e3a18790def432390ed
Ethan Lusterman <ethan.lusterman@gmail.com>
2013-10-21 23:17:46 -0400

    added pyItunes, converted to dict format for mongoDB

---
+++ itunes.py
@@ -0,0 +1,40 @@
+#!ENV/bin/python
+
+from pyItunes import *
+from pprint import pprint
+
+l = Library("data/library.xml")
+
+songs = []
+
+for id,song in l.songs.items():
+    songs.append(
+            {
+                "name": song.name,
+                "artist": song.artist,
+                "album_artist": song.album_artist,
+                "composer": song.composer,
+                "album": song.album,
+                "genre": song.genre,
+                "kind": song.kind,
+                "size": song.size,
+                "total_time": song.total_time,
+                "track_number": song.track_number,
+                "year": song.year,
+                "date_modified": song.date_modified,
+                "date_added": song.date_added,
+                "bit_rate": song.bit_rate,
+                "sample_rate": song.sample_rate,
+                "comments": song.comments,
+                "rating": song.rating,
+                "album_rating": song.album_rating,
+                "play_count": song.play_count,
+                "location": song.location,
+                "compilation": song.compilation,
+                "grouping": song.grouping,
+                "lastplayed": song.lastplayed,
+                "length": song.length
+                }
+            )
+
+pprint(songs)
