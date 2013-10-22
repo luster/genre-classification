847dcf9d9354a394f4482ce0798e13d6a8278d1b
Ethan Lusterman <ethan.lusterman@gmail.com>
2013-10-22 00:03:09 -0400

    fixed error in sample_config, modified itunes lib collection

---
+++ CONFIG.py
@@ -0,0 +1,22 @@
+#!ENV/bin/python
+
+# put API keys here
+
+lastFm = {
+        "apiKey": '820099ad6b269eb2e727b19d39515869',
+        "secret": '8864c75d0d262a5e7826420f4d73c133'
+        }
+
+
+echonest = {
+        "apiKey": 'FU4VH4MIDJDMNZ8ZC',
+        "consumerKey": '359e7989a1c695a9760c41882ce671b3',
+        "sharedSecret": 'USBC/w5HSNCjpa7a433qmA'
+        }
+
+
+# database information for mongo
+
+DATABASE_HOST = 'localhost'
+DATABASE_PORT = 27017
+DATABASE_NAME = 'genre'
