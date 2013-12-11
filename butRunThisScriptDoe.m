a880617c6c2eccbceedc8c5d0e3f2a7a38fd72bb
Robert Gruener <gruene@cooper.edu>
2013-12-11 11:56:59 -0500

    updated

--- butRunThisScriptDoe.m
+++ butRunThisScriptDoe.m
@@ -1,15 +1,17 @@
-%clear all
+clear all
 close all
 clc
 
-% fprintf('Loading training data.\n');
-% trainData = loadData('training');
-% fprintf('Loaded train data.\nLoading test data.\n');
-% testData = loadData('testing');
-% fprintf('Loaded test data.\nAdding covariance features.\n');
-
-load matlab/data.mat
-% load paperDataReal.mat
+if exist(fullfile(cd, 'data.mat'), 'file') ~= 2
+  fprintf('Loading training data.\n');
+  trainData = loadData('training');
+  fprintf('Loaded train data.\nLoading test data.\n');
+  testData = loadData('testing');
+  fprintf('Loaded test data.\nAdding covariance features.\n');
+  save('data.mat','trainData','testData');
+else
+  load data.mat
+end
 
 trainData = addCovFeature(trainData);
 testData = addCovFeature(testData);
