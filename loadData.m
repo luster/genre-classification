41656f4acb2a97a043cb555bee56b5f9961c155b
Robert Gruener <gruene@cooper.edu>
2013-12-11 16:59:49 -0500

    updated

--- loadData.m
+++ loadData.m
@@ -13,16 +13,20 @@ for ii=1:numGenres
     g(ii).files = d(3:end);
     for jj=3:length(d)
         sstr = sprintf('%s/%s',str,d(jj).name);
-        [x,fs]=audioread(sstr);
-        x = (x(:,1)+x(:,2))/2;
-        
-        [ceps,~,~]=melfcc(x,fs,'numcep',20,'minfreq',0,'maxfreq',2000);
-        ceps = ceps';
-        ceps(:,1:13);
-        
-        cep_feat = mean(ceps);
-        g(ii).files(jj-2).ceps = ceps;
-        g(ii).files(jj-2).features = cep_feat;
+        try
+            [x,fs]=audioread(sstr);
+            x = (x(:,1)+x(:,2))/2;
+
+            [ceps,~,~]=melfcc(x,fs,'numcep',20,'minfreq',0,'maxfreq',2000);
+            ceps = ceps';
+            ceps(:,1:13);
+
+            cep_feat = mean(ceps);
+            g(ii).files(jj-2).ceps = ceps;
+            g(ii).files(jj-2).features = cep_feat;
+        catch
+            fprintf('Likely Corrupted File found at %s. Please delete it.', sstr);
+        end
     end
 end
 end
